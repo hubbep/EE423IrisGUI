755aa28b16ff3a965f3fca86ebc61c8844e206d3
Mike-243 <lema@clarkson.edu>
2022-04-24 16:56:14 -0400

    added accept function for when masks are good

--- PySimpleGUI-4.56.0/PySimpleGUI/IrisGUI.py
+++ PySimpleGUI-4.56.0/PySimpleGUI/IrisGUI.py
@@ -93,7 +93,6 @@ class ProgramInitialize:
             print("Problem reading folder locations")
             exit(2)
 
-
 class OvalMask:
     """
     CLASS TO ACCESS MASK DATA AND LOGIC
@@ -142,22 +141,6 @@ class OvalMask:
         self.plotID = graph.draw_oval(start_point, end_point, line_color='red')
         return self.plotID
 
-
-class DragHistory:
-    def __init__(self, point_size=None):
-        self.dragPoints = tuple()
-        self.pointSize = point_size
-
-    def add_point(self, point=tuple()):
-        self.dragPoints = self.dragPoints + point
-
-
-class PointsHistory:
-    def __init__(self):
-        self.dragPaths = None
-
-    def add_path(self, drag_path=None):
-        self.dragPaths = self.dragPaths + drag_path
 
 # COULD POTENTIALLY IMPLEMENT ARC MASK TO MASK EYELIDS
 # class arcMask():
@@ -236,15 +219,12 @@ class ImageProcessing:
             self.img = self.img.resize((int(cur_width * self.scale), int(cur_height * self.scale)), PIL.Image.ANTIALIAS)
 
     def draw_image_raw(self):
-        graph.change_coordinates((0, 0), G_SIZE)
         self.get_img()
-        cur_width, cur_height = self.img.size
         if self.resize:
             self.resize_img()
         bio = io.BytesIO()
         self.img.save(bio, format="PNG")
         self.plotID = graph.draw_image(data=bio.getvalue(), location=(0, G_SIZE[1]))
-        graph.change_coordinates((0, (cur_height - G_SIZE[1])), (G_SIZE[0], cur_height))
         return self.plotID
 
     def draw_seg_image_raw(self):
@@ -269,10 +249,9 @@ class ImageProcessing:
         self.get_isfile()
         filename_prefix = os.path.basename(self.file)
         # filename_prefix = os.path.splitext(filename_prefix)[0]
-        filename_suffix = "reviewed.jpg"
+        # filename_suffix = "mask.jpg"
         # filename = os.path.join(program.maskFolder, filename_prefix + "_" + filename_suffix)
         filename = os.path.join(program.maskFolder, filename_prefix)
-        filename2 = os.path.join(program.maskFolder, filename_prefix + "_" + filename_suffix)
         cur_width, cur_height = self.img.size
 
         for k in range(0, cur_height, 1):
@@ -290,7 +269,6 @@ class ImageProcessing:
         new_image = Image.fromarray(self.array)
         new_image = new_image.resize(size=(image_id.org_width, image_id.org_height))
         new_image.save(filename)
-        new_image.save(filename2)
 
     # def img_init_config(self):
     #     for section in Config.sections():
@@ -345,24 +323,22 @@ col = [[sg.T('SEGMENTATION PREVIEW', enable_events=True)],
        [graphseg],
        [sg.T('MASK PREVIEW', enable_events=True)],
        [graphmask],
+       [sg.T('GRAPH TOOLS', enable_events=True)],
+       [sg.R('Draw oval - inner iris', 1, key='-IN-OVAL-', enable_events=True)],
+       [sg.R('Draw oval - outer iris', 1, key='-OUT-OVAL-', enable_events=True)],
+       [sg.R('Erase - inner iris', 1, key='-ERASE-INNER-', enable_events=True)],
+       [sg.R('Erase - outer iris', 1, key='-ERASE-OUTER-', enable_events=True)],
+       [sg.R('Erase all', 1, key='-CLEAR-', enable_events=True)],
+       # [sg.R('Send to back', 1, key='-BACK-', enable_events=True)],
+       # [sg.R('Bring to front', 1, key='-FRONT-', enable_events=True)],
+       # [sg.R('Move Stuff', 1, key='-MOVE-', enable_events=True)],
+       [sg.T('FILE TOOLS', enable_events=True)],
+       [sg.B('Previous Image', key='-PREV-'), sg.B('Next Image', key='-NEXT-')],
+       [sg.B('Save Mask', key='-SAVE-')], [sg.B('Accept Mask', key='-ACCEPT-')],
        ]
 
-col2 = [[sg.T('GRAPH TOOLS', enable_events=True)],
-        [sg.R('Draw oval - inner iris', 1, key='-IN-OVAL-', enable_events=True)],
-        [sg.R('Draw oval - outer iris', 1, key='-OUT-OVAL-', enable_events=True)],
-        [sg.R('Erase - inner iris', 1, key='-ERASE-INNER-', enable_events=True)],
-        [sg.R('Erase - outer iris', 1, key='-ERASE-OUTER-', enable_events=True)],
-        [sg.R('Erase all', 1, key='-CLEAR-', enable_events=True)],
-        # [sg.R('Send to back', 1, key='-BACK-', enable_events=True)],
-        # [sg.R('Bring to front', 1, key='-FRONT-', enable_events=True)],
-        # [sg.R('Move Stuff', 1, key='-MOVE-', enable_events=True)],
-        [sg.T('FILE TOOLS', enable_events=True)],
-        [sg.B('Previous Image', key='-PREV-'), sg.B('Next Image', key='-NEXT-')],
-        [sg.B('Save Mask', key='-SAVE-')],
-        ]
-
 layout = [[sg.Text(f'Displaying image: '), sg.Text(k='-FILENAME-'), sg.Text('', k='-DIMERROR-')],
-          [graph, sg.Column(col), sg.Column(col2)],
+          [graph, sg.Column(col)],
           [sg.Text('Developed for Clarkson Universtiy Biometric Department'), sg.Text(key='info', size=(60, 1))]]
 window = sg.Window('Iris correction program', layout, margins=(0, 0), use_default_focus=False, finalize=True)
 
@@ -390,8 +366,6 @@ start_point = end_point = prior_rect = None
 while True:
     event: object
     event, values = window.read()
-    # This is cool but how is order determined?
-    print(event, values)
     if event == sg.WIN_CLOSED:
         break
 
@@ -440,6 +414,17 @@ while True:
         cfgfile = open(config_file_location, 'w')
         Config.write(cfgfile)
         cfgfile.close()
+
+    if event == '-ACCEPT-':
+        print("Mask Accepted")
+        f_loc = mask_image_id.file
+        f_name = os.path.basename(mask_image_id.file)
+        split_f_name = os.path.splitext(f_name)
+        new_f_name = split_f_name[0]+"_accepted"
+        recon_f_name = f_loc.replace(split_f_name[0], new_f_name)
+
+        os.rename(f_loc,recon_f_name)
+        #print(mask_image_id.file)
 
     if event == '-IN-OVAL-':
         tool = '-IN-OVAL-'
